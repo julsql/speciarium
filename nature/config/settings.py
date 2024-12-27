334ba5d598494f53fd0d06fee29ba82ed4a9b5b1
Juliette Debono <juliette.debono@telecom-sudparis.eu>
2024-12-27 01:29:47 +0100

    docker

--- nature/config/settings.py
+++ nature/config/settings.py
@@ -106,6 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://especes.julsql.fr',  # Remplacez par votre domaine de production
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
