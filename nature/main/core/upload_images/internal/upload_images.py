9a77d553dde7c29bd94d417befaf639f65c50a6b
Juliette Debono <juliette.debono@telecom-sudparis.eu>
2025-02-03 21:52:50 +0100

    logger

--- nature/main/core/upload_images/internal/upload_images.py
+++ nature/main/core/upload_images/internal/upload_images.py
@@ -57,18 +57,22 @@ def get_latin_name(image_path):
 
 def delete_images(images_to_delete):
     for image_key in images_to_delete:
-        image_path, image_hash = image_key.split(":")
-        latin_name = get_latin_name(image_path)
+        try:
+            image_path, image_hash = image_key.split(":")
+            latin_name = get_latin_name(image_path)
 
-        Photos.objects.filter(hash=image_hash).delete()
-        specie_id = Species.objects.filter(latin_name=latin_name).values_list('id', flat=True).first()
-        if specie_id and not Photos.objects.filter(specie_id=specie_id).exists():
-                Species.objects.filter(id=specie_id).delete()
+            Photos.objects.filter(hash=image_hash).delete()
+            specie_id = Species.objects.filter(latin_name=latin_name).values_list('id', flat=True).first()
+            if specie_id and not Photos.objects.filter(specie_id=specie_id).exists():
+                    Species.objects.filter(id=specie_id).delete()
 
-        image_small = os.path.join(SMALL_PATH, image_path)
-        image_vignette = os.path.join(VIGNETTE_PATH, image_path)
-        delete_file_with_permission_check(image_small)
-        delete_file_with_permission_check(image_vignette)
+            image_small = os.path.join(SMALL_PATH, image_path)
+            image_vignette = os.path.join(VIGNETTE_PATH, image_path)
+            delete_file_with_permission_check(image_small)
+            delete_file_with_permission_check(image_vignette)
+        except Exception as e:
+            logger.error(f"Failed to delete image {image_key}")
+            logger.error(e)
 
 
 def delete_file_with_permission_check(file_path):
