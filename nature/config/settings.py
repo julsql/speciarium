d86e974f8a9a3fb95ce7b43b2173fb9fe4f2401d
Juliette Debono <juliette.debono@telecom-sudparis.eu>
2025-03-15 19:01:49 +0100

    websocket config

--- nature/config/settings.py
+++ nature/config/settings.py
@@ -80,20 +80,22 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'config.asgi.application'
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [('127.0.0.1', 6379)],
-#         },
-#     },
-# }
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    },
-}
+# Configurer le Channel Layer en fonction de l'environnement
+if config('DJANGO_ENV') == 'production':
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [('redis', 6379)],
+            },
+        },
+    }
+else:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+        },
+    }
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
