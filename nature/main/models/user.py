05d2dc35a8fdd02199567a29d86a95711d8bb31c
Jul SQL <julsql1@gmail.com>
2025-06-10 23:02:02 +0200

    feat(add-users): Ajout des entités en base de données

--- nature/main/models/user.py
+++ nature/main/models/user.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class AppUser(AbstractUser):
     email = models.EmailField(max_length=254)
+    current_collection = models.ForeignKey(
+        'main.Collection',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='current_users'
+    )
