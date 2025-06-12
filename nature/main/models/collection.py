cd7edf6a461ccff3357198e7c102b1f096e46dad
Jul SQL <julsql1@gmail.com>
2025-06-12 22:34:23 +0200

    feat(upload): Seul le propriétaire d'une collection peut ajouter des photos

--- nature/main/models/collection.py
+++ nature/main/models/collection.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 
+from main.models import AppUser
+
 
 class Collection(models.Model):
     title = models.CharField(max_length=100)
@@ -8,6 +10,7 @@ class Collection(models.Model):
         settings.AUTH_USER_MODEL,
         related_name="collections"
     )
+    owner = models.ForeignKey(AppUser, null=True, on_delete=models.CASCADE, related_name="collections_owned")
 
     def __str__(self) -> str:
         return self.title
