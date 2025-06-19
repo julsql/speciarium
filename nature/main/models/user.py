c4694bdf17e2620368f91efa44ced06ced67b1e3
Jul SQL <julsql1@gmail.com>
2025-06-19 18:50:09 +0200

    feat(models): current_collection non nulle

--- nature/main/models/user.py
+++ nature/main/models/user.py
@@ -5,16 +5,12 @@ from main.models.map_tiles import MapTiles
 
 
 class AppUser(AbstractUser):
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254, unique=True)
     map_tiles = models.ForeignKey(MapTiles,
-                                  null=True,
-                                  blank=True,
-                                  on_delete=models.CASCADE,
+                                  on_delete=models.PROTECT,
                                   related_name="users")
     current_collection = models.ForeignKey(
         'main.Collection',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.PROTECT,
         related_name='current_users'
     )
