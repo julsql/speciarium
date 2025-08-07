7a424f540b0360470fe30dbfcd1eb95530dda92d
Jul SQL <julsql1@gmail.com>
2025-08-07 20:25:57 +0200

    Revert "wip: log debug"
    This reverts commit e35aac41a65964d545727a21a03b93ed05458fa4.

--- nature/main/static/main/assets/scripts/upload_images.js
+++ nature/main/static/main/assets/scripts/upload_images.js
@@ -260,9 +260,6 @@ folderInput.addEventListener("change", async (event) => {
     let currentIndex = 0;
 
     socket.onmessage = function (event) {
-        console.log(event.data)
-        console.log(currentIndex)
-        console.log(resizedFiles.length)
         const data = JSON.parse(event.data);
         if (data.progress === "Done" && currentIndex >= resizedFiles.length) {
             socket.close();
@@ -288,7 +285,7 @@ folderInput.addEventListener("change", async (event) => {
         info.textContent = `Images ajoutées`;
         progressBar.style.width = '0';
         progressBarContainer.style.display = "none";
-        // location.reload();
+        location.reload();
     };
 
     loading.style.display = "block";
