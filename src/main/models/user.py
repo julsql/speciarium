01e6e6d4eeb40281f1a72b59c55ff62744552e22
Jul SQL <julsql1@gmail.com>
2025-11-27 00:45:09 +0100

    refacto: migrations propres

--- src/main/models/user.py
+++ src/main/models/user.py
@@ -1,18 +1,27 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 from main.models.map_tiles import MapTiles
 from main.models.theme import Theme
 
+from django.db import models
+
 
 class AppUser(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
-    map_tiles = models.ForeignKey(MapTiles,
-                                  on_delete=models.PROTECT,
-                                  related_name="users")
-    theme = models.ForeignKey(Theme,
-                              on_delete=models.PROTECT,
-                              related_name="users")
+    map_tiles = models.ForeignKey(
+        MapTiles,
+        on_delete=models.PROTECT,
+        related_name="users",
+        null=True,
+        blank=True
+    )
+    theme = models.ForeignKey(
+        Theme,
+        on_delete=models.PROTECT,
+        related_name="users",
+        null=True,
+        blank=True
+    )
     current_collection = models.ForeignKey(
         'main.Collection',
         on_delete=models.PROTECT,
